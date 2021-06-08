60363f68c85bf3ac338a93b2651c85fef68328a8
Jan Kołodziej <76879087+JanKolo04@users.noreply.github.com>
2021-06-08 19:02:26 +0200

    menu button

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -10,4 +6,26 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
 }
