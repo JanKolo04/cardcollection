8fc389353e6f4800dccca3f7d218cfc21ad7dc48
Jan Kołodziej <76879087+JanKolo04@users.noreply.github.com>
2021-06-28 17:24:01 +0200

    Add files via upload

---
+++ additional files/categories/script.js
@@ -0,0 +1,118 @@
+//Ability
+function myFunctionAbility() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moreability");
+  var btnText = document.getElementById("btnability");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Ally
+function myFunctionAlly() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moreally");
+  var btnText = document.getElementById("btnally");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Equipment
+function myFunctionEquipment() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moreequipment");
+  var btnText = document.getElementById("btnequipment");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Hero
+function myFunctionHero() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("morehero");
+  var btnText = document.getElementById("btnhero");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Location
+function myFunctionLocation() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("morelocation");
+  var btnText = document.getElementById("btnlocation");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Master Hero
+function myFunctionMaster() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("moremaster");
+  var btnText = document.getElementById("btnmaster");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
+
+//Quest
+function myFunctionQuest() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("morequest");
+  var btnText = document.getElementById("btnquest");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "↓";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "↑";
+    moreText.style.display = "inline";
+  }
+}
